c1cef462ace1f82866968a898cfc2cd9a1a961a8
Tagline-Sahil <sbr@taglineinfotech.com>
2025-11-21 12:22:55 +0530

    feat(turbo-module): implement NativeLocalStorage TurboModule IOS
    Add new TurboModule for persistent storage using NSUserDefaults on iOS
    Enable new architecture by setting RCTNewArchEnabled to true
    Update project configuration and dependencies to support new module

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.mm
@@ -0,0 +1,51 @@
+//
+//  RCTNativeLocalStorage.m
+//  turboModuleDemo
+//
+//  Created by mac on 21/11/25.
+//
+
+#import "RCTNativeLocalStorage.h"
+
+static NSString *const RCTNativeLocalStorageKey = @"local-storage";
+
+@interface RCTNativeLocalStorage()
+@property (strong, nonatomic) NSUserDefaults *localStorage;
+@end
+
+
+@implementation RCTNativeLocalStorage
+
+RCT_EXPORT_MODULE(NativeLocalStorage)
+
+- (id) init {
+  if (self = [super init]) {
+    _localStorage = [[NSUserDefaults alloc] initWithSuiteName:RCTNativeLocalStorageKey];
+  }
+  return self;
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params { 
+  return std::make_shared<facebook::react::NativeLocalStorageSpecJSI>(params);
+}
+
+- (void)clear { 
+  NSDictionary *keys = [self.localStorage dictionaryRepresentation];
+    for (NSString *key in keys) {
+      [self removeItem:key];
+    }
+}
+
+- (NSString * _Nullable)getItem:(nonnull NSString *)key { 
+  return [self.localStorage stringForKey:key];
+}
+
+- (void)removeItem:(nonnull NSString *)key { 
+  [self.localStorage removeObjectForKey:key];
+}
+
+- (void)setItem:(nonnull NSString *)value key:(nonnull NSString *)key { 
+  [self.localStorage setObject:value forKey:key];
+}
+
+@end
